e1cc787ec276fca47e35af130d15ee6a068a3e21
haosdent <haosdent@gmail.com>
2014-12-27 18:39:00 +0800

    Add INIConfig#load method.

--- src/haosdent/config/iniconfig.go
+++ src/haosdent/config/iniconfig.go
@@ -1,7 +1,10 @@
 package config
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
 type INIConfig struct {
@@ -9,12 +12,23 @@ type INIConfig struct {
 	props map[string]string
 }
 
+type ParseState int
+
+const (
+	START ParseState = iota
+	NORMAL
+	QUOTED
+	ESCAPE
+	STOP
+)
+
 func NewINIConfig(path string) *INIConfig {
-	var props = make(map[string]string, 100)
-	return &INIConfig{
+	var instance = &INIConfig{
 		path,
-		props,
+		make(map[string]string, 100),
 	}
+	instance.load()
+	return instance
 }
 
 func (self *INIConfig) Get(k string) (val string, err error) {
@@ -29,4 +43,120 @@ func (self *INIConfig) Get(k string) (val string, err error) {
 
 func (self *INIConfig) AddProp(k string, v interface{}) {
 	self.props[k] = v.(string)
+}
+
+func (self *INIConfig) isCommentLine(line string) bool {
+	if line == "" {
+		return false
+	}
+
+	return line[0] == '#' || line[0] == ';'
+}
+
+func (self *INIConfig) isSectionLine(line string) bool {
+	if line == "" {
+		return false
+	}
+
+	return line[0] == '[' && line[len(line)-1] == ']'
+}
+
+func (self *INIConfig) isSpace(c rune) bool {
+	if c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r' {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (self *INIConfig) parseVal(val string) string {
+	if len(val) == 0 {
+		return val
+	}
+
+	var state = START
+	var bs = make([]rune, len(val))
+	var bl = 0
+	var quote rune
+
+	for _, c := range val {
+		switch state {
+		case START:
+			if c == '"' || c == '\'' {
+				quote = c
+				state = QUOTED
+			} else if c == '#' || c == ';' {
+				state = STOP
+			} else if !self.isSpace(c) {
+				bs[bl] = c
+				bl++
+				state = NORMAL
+			}
+		case NORMAL:
+			if c == '#' || c == ';' || self.isSpace(c) {
+				state = STOP
+			} else {
+				bs[bl] = c
+				bl++
+			}
+		case QUOTED:
+			if c == '\\' {
+				state = ESCAPE
+			} else if c == quote {
+				state = STOP
+			} else {
+				bs[bl] += c
+				bl++
+			}
+		case ESCAPE:
+			if c != quote {
+				bs[bl] += '\\'
+				bl++
+			}
+			bs[bl] += c
+			bl++
+			state = QUOTED
+		case STOP:
+			break
+		}
+	}
+
+	val = string(bs[:bl])
+
+	return val
+}
+
+func (self *INIConfig) load() {
+	var file, _ = os.Open(self.path)
+	defer file.Close()
+
+	var scanner = bufio.NewScanner(file)
+	var section = ""
+	for scanner.Scan() {
+		var line = scanner.Text()
+		line = strings.TrimSpace(line)
+		if self.isCommentLine(line) || len(line) == 0 {
+			continue
+		} else if self.isSectionLine(line) {
+			section = line[1:len(line)-1] + "."
+			continue
+		}
+
+		var index = strings.Index(line, "=")
+		if index < 0 {
+			index = strings.Index(line, ":")
+		}
+
+		if index < 0 {
+			var key = section + line
+			self.props[key] = ""
+		} else {
+			var key = line[:index]
+			key = section + strings.TrimSpace(key)
+			var val = line[index+1:]
+			val = self.parseVal(val)
+			self.props[key] = val
+			//fmt.Println(key, val)
+		}
+	}
 }
--- src/haosdent/config/iniconfig_test.go
+++ src/haosdent/config/iniconfig_test.go
@@ -1,11 +1,35 @@
 package config
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
 	"testing"
 )
 
+func SetUpConfigFile(path string) {
+	var data = []byte(`
+    [props]
+    exist = true
+    `)
+	var err = ioutil.WriteFile(path, data, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TearDownConfigFile(path string) {
+	var err = os.Remove(path)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func TestGet(t *testing.T) {
-	var path = "/tmp/config.ini"
+	var path = "config.ini"
+	SetUpConfigFile(path)
+	defer TearDownConfigFile(path)
+
 	var c Configer = NewINIConfig(path)
 	var key = "props.exist"
 
@@ -13,9 +37,13 @@ func TestGet(t *testing.T) {
 	// c.AddProp(key, except)
 	var val, err = c.Get(key)
 	if err != nil {
-		t.Errorf("[after]Error is not empty.")
+		t.Errorf("[after]Error is not empty: %s.", err)
 	}
 	if val != except {
+		fmt.Println(len(val), len(except))
+		for _, c := range val {
+			fmt.Println(c)
+		}
 		t.Errorf("[after]Value \"%s\" is not equal to except \"%s\"", val, except)
 	}
 
@@ -32,8 +60,11 @@ func TestGet(t *testing.T) {
 
 func TestAddProp(t *testing.T) {
 	var path = "/tmp/config.ini"
+	SetUpConfigFile(path)
+	defer TearDownConfigFile(path)
+
 	var c Configer = NewINIConfig(path)
-	var key = "props.exist"
+	var key = "props.noexist"
 
 	var val, err = c.Get(key)
 	if err == nil {
@@ -48,7 +79,7 @@ func TestAddProp(t *testing.T) {
 	c.AddProp(key, val)
 	val, err = c.Get(key)
 	if err != nil {
-		t.Errorf("[after]Error is not empty.")
+		t.Errorf("[after]Error is not empty: %s.", err)
 	}
 	if val != except {
 		t.Errorf("[after]Value \"%s\" is not equal to except \"%s\"", val, except)
