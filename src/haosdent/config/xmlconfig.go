af06b461921271bb932c604a99a99fe82621afd6
haosdent <haosdent@gmail.com>
2015-01-22 10:01:59 +0800

    Update XmlConfig.

---
+++ src/haosdent/config/xmlconfig.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+)
+
+type XmlConfig struct {
+    path  string
+    props map[string][]string
+}
+
+func NewXmlConfig(path string) *XmlConfig {
+    var instance = &XmlConfig{
+        path,
+        make(map[string][]string, 100),
+    }
+    var err = instance.load()
+    if err != nil {
+        //panic(err)
+        return nil
+    } else {
+        return instance
+    }
+}
+
+func (self *XmlConfig) Get(k string) (val string, err error) {
+    var ok bool
+    var l []string
+    if l, ok = self.props[k]; ok {
+        err = nil
+        val = strings.Join(l, ",")
+    } else {
+        err = fmt.Errorf("Don't contains key: \"%s\".", k)
+    }
+    return val, err
+}
+
+func (self *XmlConfig) AddProp(k string, v interface{}) {
+    var ok bool
+    var l []string
+    if l, ok = self.props[k]; !ok {
+        l = make([]string, 0, 3)
+    }
+    k = strings.TrimSpace(k)
+    if (len(k)) == 0 {
+        return
+    }
+
+    var vStr = strings.TrimSpace(v.(string))
+    l = append(l, vStr);
+    self.props[k] = l;
+}
